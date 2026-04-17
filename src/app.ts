8666c081682befec1bef609b745714e3c8e303b3
Akash Sharma <akash.sharma@techaheadcorp.com>
2026-04-17 15:48:38 +0530

    auth api initialised

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,6 @@ import cors from "cors";
 import helmet from "helmet";
 import morgan from "morgan";
 import authRoutes from "./modules/auth/auth.routes";
-import paymentsRoutes from "./modules/payments/payments.routes";
 const app = express();
 
 app.use(cors());
@@ -15,6 +14,5 @@ app.get("/health", (req, res) => {
   res.json({ status: "OK" });
 });
 app.use("/api/auth", authRoutes);
-app.use("/", paymentsRoutes);
 
 export default app;
